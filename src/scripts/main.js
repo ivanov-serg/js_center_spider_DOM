622afb29adea2c1e7ddfedb39c294bf9d9dee415
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-04-22 20:23:34 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+window.addEventListener('load', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  const leftOffset = (wallRect.width - spiderRect.width) / 2;
+  const topOffset = (wallRect.height - spiderRect.height) / 2;
+
+  spider.style.position = 'absolute';
+  spider.style.left = `${leftOffset}px`;
+  spider.style.top = `${topOffset}px`;
+});
