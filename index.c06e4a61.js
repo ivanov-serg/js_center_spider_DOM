d0d91d3f2175ce8f5f688839a1abb9a0aaa00c40
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-04-22 21:26:20 +0300

    make build

---
+++ index.c06e4a61.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",function(){var t=document.querySelector(".wall"),e=document.querySelector(".spider"),i=t.clientWidth,l=t.clientHeight,n=e.clientWidth,c=e.clientHeight;e.style.position="absolute",e.style.left="".concat((i-n)/2,"px"),e.style.top="".concat((l-c)/2,"px")});
+//# sourceMappingURL=index.c06e4a61.js.map
